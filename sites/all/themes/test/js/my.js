206ed216fe8af01fddf27cdee9fafda7a92f13ec
lyra007 <elfishing07@gmail.com>
2018-02-02 10:34:41 +0800

    one bug fixed

--- sites/all/themes/test/js/my.js
+++ sites/all/themes/test/js/my.js
@@ -1,3 +1,30 @@
-/**
- * Created by lyra on 31/01/2018.
- */
+// $(function () {
+//     var App=new Vue({
+//         el:'#App',
+//         data:{
+//             aaa:[
+//                 {
+//                     "id":"1"
+//                 },
+//                 {
+//                     "id":"2"
+//                 },
+//                 {
+//                     "id":"3"
+//                 }
+//             ]
+//         },
+//         methods:{
+//
+//         }
+//
+//
+//     });
+//     $("#App").on("click",".box ul li button",function () {
+//         $(this).parent("li").siblings("li").animate({width:"80px"},500);
+//         $(this).parent("li").animate({width:"450px"},500);
+//     });
+//     $("#App").on("click",".box ul li input",function () {
+//         $(this).parent("li").animate({width:"80px"},500);
+//     });
+// })
